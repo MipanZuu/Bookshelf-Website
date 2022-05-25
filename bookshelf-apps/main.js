fe4f75f2a7ab99d743c6929a9124e07c1393509c
Denta Hidayat <denta.dent53@gmail.com>
2022-05-25 08:09:34 +0700

    Try to use back end and store to database

---
+++ bookshelf-apps/main.js
@@ -0,0 +1,35 @@
+const input = document.querySelector(".input_section");
+
+const addBook = document.querySelector("#inputBookTitle");
+const addAuthor = document.querySelector("#inputBookAuthor");
+const addYear = document.querySelector("#inputBookYear");
+const readBook = document.querySelector("#inputBookIsComplete");
+const inputSubmit = document.querySelector("#inputBookIsComplete");
+const search = document.querySelector("#searchBookTitle");
+const searchSubmit = document.querySelector("#searchSubmit");
+
+let myLibrary = [];
+
+function addTodo() {
+    const addBook = document.querySelector("#inputBookTitle");
+    const addAuthor = document.querySelector("#inputBookAuthor");
+    const addYear = document.querySelector("#inputBookYear");
+    const readBook = document.querySelector("#inputBookIsComplete");
+    const inputSubmit = document.querySelector("#inputBookIsComplete");
+    const search = document.querySelector("#searchBookTitle");
+    const searchSubmit = document.querySelector("#searchSubmit");
+    console.log("todo" + textTodo);
+    console.log("timestamp" + timestamp);
+}
+
+class Book {
+    constructor()
+}
+
+function addBook () {
+
+}
+
+function submitBook () {
+    
+}
